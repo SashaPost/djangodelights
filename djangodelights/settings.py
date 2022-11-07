a839956c8596b37ffbc7cfdf0ed26aa85e247859
SashaPost <postilha.a@gmail.com>
2022-11-07 23:04:46 +0200

    changed settings again

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', \
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 # DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
