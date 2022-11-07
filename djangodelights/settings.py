4dca7ae429037ce7efc9f4e7062a890610af9a22
SashaPost <postilha.a@gmail.com>
2022-11-07 22:35:15 +0200

    Hope added some fixes (already mad)

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', \
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = False
 
 ALLOWED_HOSTS = []
 
